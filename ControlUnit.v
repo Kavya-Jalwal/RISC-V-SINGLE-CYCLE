4980908c39d25cae3a992683d1971d2dbdfeb8e0
Kavya <kavya.ec.23@nitj.ac.in>
2026-06-06 11:08:30 +0530

    Create ControlUnit.v

---
+++ ControlUnit.v
@@ -0,0 +1,26 @@
+module Control_Unit(instruction,Branch,MemRead,MemtoReg,ALUOp,MemWrite,ALUSrc,RegWrite);
+input [6:0] instruction;
+output reg Branch,MemRead,MemtoReg,MemWrite,ALUSrc,RegWrite;
+output reg [1:0] ALUOp;
+always @(*)
+begin
+   case(instruction)
+   7'b0110011: {ALUSrc , MemtoReg , RegWrite, MemRead, MemWrite,Branch,ALUOp} <=8'b001000_10;
+   7'b0000011: {ALUSrc , MemtoReg , RegWrite, MemRead, MemWrite,Branch,ALUOp} <=8'b111100_00;
+   7'b0100011: {ALUSrc , MemtoReg , RegWrite, MemRead, MemWrite,Branch,ALUOp} <=8'b100010_00;
+   7'b1100011: {ALUSrc , MemtoReg , RegWrite, MemRead, MemWrite,Branch,ALUOp} <=8'b000001_01;
+   default:
+    begin
+    ALUSrc   = 0;
+    MemtoReg = 0;
+    RegWrite = 0;
+    MemRead  = 0;
+    MemWrite = 0;
+    Branch   = 0;
+    ALUOp    = 2'b00;
+    end
+   endcase
+end
+
+   
+endmodule
